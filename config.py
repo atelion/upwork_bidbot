0815666d62f0daa8f0c9e91e23569146cda44b43
atelion <happilyai.meet@gmail.com>
2024-08-06 09:12:50 -0700

    upwork bid bot

---
+++ config.py
@@ -0,0 +1,46 @@
+OCTO_API_TOKEN = ''
+OCTO_HTTP_SERVER = 58888
+OCTO_ID = ''
+OCTO_AUTOMATION_URL = 'https://app.octobrowser.net/api/v2/automation/profiles'
+OCTO_HEADER = {
+    'Content-Type': 'application/json',
+    'X-Octo-Api-Token': OCTO_API_TOKEN
+}
+LOCAL_API = 'http://localhost:58888/api/profiles'
+
+MIN_NUMBER = 8
+UPWORK_URL = "https://www.upwork.com/nx/signup/"
+MESSAGE_URL = "https://www.upwork.com/ab/messages/rooms/"
+FIRST_NAME = ""
+LAST_NAME = ""
+EMAIL = "" #robert.wilk.proa in case of robert.wilk.proa@proton.me
+PASSWORD = "" #upwork password
+
+SUMMARY = "Innovative and creative AI / Embedded Software Engineer with 10 years of experience in circuit design, application design, development, testing and deployment. Highly experienced in bare-metal programming, free rtos, signal processing and AI model training and deployment. Rich knowledge in Software engineering, Electronic engineering, Control Engineering. Rich experience in Altium Designer, IAR, KEIL and AWS(especially sagemaker and ec2), GCP. Master several programming languages such as C, C++, C#, Python, Go, and modern script languages such as React, Vue, Node, etc."
+
+PROXY = {
+    "type": "socks5",
+    "host": "1.1.1.1",
+    "port": 12324,
+    "login": "14a5eb9f4da5c",
+    "password": "44a344b45d"
+}
+
+PHOTO_PATH = R'E:\Personal Data\Personal information\My Picture\Robert.jpg'
+RESUME_PATH = R'E:\Personal Data\Personal information\ID, Passport, driver license\Robert Wilk\RobertWilkResume.pdf'
+PROFESSIONAL_ROLE = 'Software Engineer'
+SKILLS_LIST = ['','Python', 'C#', 'Java', 'JavaScript', 'Artificial Intelligence', 'Machine Learning', 'Computer Vision', 'Natural Language Processing', 'Docker', 'Amazon Web Services', 'Google Cloud Platform', 'Web Development', 'Mobile App', 'Deep Learning', 'Unity']
+CATEGORIES= {
+    'Data Science & Analytics': ['AI & Machine Learning','Data Analysis & Testing'],
+    'Engineering & Architecture': ['Electrical & Electronic Engineering', 'Energy & Mechanical Engineering'],
+    'Web, Mobile & Software Dev':['Desktop Application Development', 'Game Design & Development', 'Mobile Development', 'Other - Software Development', 'Web & Mobile Design', 'Web Development']
+}
+
+HOURLY_RATE = 30
+
+COUNTRY = ''
+STREET_ADDRESS = ''
+CITY = ''
+STATE = ''
+POSTAL_CODE = ''
+PHONE_NUMBER = ''
